d9ceedeb765a49d3dd964ba15ba9e493c4eac2aa
jcgu <jcgu@google.com>
2026-06-04 00:48:30 -0700

    Add JAX raiden_transfer_engine.pyi interface stub
    PiperOrigin-RevId: 926525573

--- api/jax/transfer_engine.pyi
+++ api/jax/transfer_engine.pyi
@@ -1,3 +1,19 @@
+# Copyright 2026 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Interface definitions for JAX TransferEngine."""
+
 class TransferEngine:
   def __init__(
       self,
@@ -23,3 +39,5 @@ class TransferEngine:
       local_block_ids: list[int],
   ) -> int: ...
   def complete_read(self) -> tuple[list[str], list[str], list[str]]: ...
+
+RaidenTransferEngine = TransferEngine
